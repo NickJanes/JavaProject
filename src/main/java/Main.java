aa16d1c55d1633bb6b5ffa471ce6e410162fb205
NickJanes <nickjanes1998@gmail.com>
2020-10-08 15:23:50 -0700

    Added default project with main file

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+/*
+
+Main.java
+
+Author: Nicholas Janes
+
+Date: October 8, 2020
+
+Description: This is the main file for assignment 07 Object Creation
+
+*/
+public class Main {
+
+    /*
+
+    Function: main
+        
+    Author: Nicholas Janes
+
+    Date: October 8, 2020
+    
+    Description: This is the main method.
+
+    Inputs: 
+
+    Outputs: 
+
+    */
+    public static void main(String args[]) {
+        
+    }
+
+
+}
